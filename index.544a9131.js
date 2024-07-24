209696c60010436c07f49a30542e5c5a021886b3
blackheaven <blackheaven@users.noreply.github.com>
2024-07-24 23:00:05 +0000

    deploy: be38e4a7179c3d88775e95fac20997643cb26f9a

---
+++ index.544a9131.js
@@ -0,0 +1,2 @@
+var o,p,n;p=(o=jQuery)(window),n=o("body"),breakpoints({wide:["1281px","1680px"],normal:["981px","1280px"],narrow:["841px","980px"],narrower:["737px","840px"],mobile:["481px","736px"],mobilep:[null,"480px"]}),p.on("load",function(){window.setTimeout(function(){n.removeClass("is-preload")},100)});
+//# sourceMappingURL=index.544a9131.js.map
